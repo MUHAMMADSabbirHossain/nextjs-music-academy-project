b35d2b09d0e69569ea44696324997163a2b43dcf
Muhammad Sabbir Hossain Bin Ahmad <dustobalok000@gmail.com>
2025-08-28 10:05:43 +0600

    Complete: Solving Hero Section challenges

---
+++ musicnextjs/src/components/HeroSection.tsx
@@ -0,0 +1,37 @@
+import Link from "next/link";
+import { Spotlight } from "./ui/Spotlight";
+import { Button } from "./ui/moving-border";
+
+function HeroSection() {
+  return (
+    <div className="h-auto md:h-[40rem] w-full rounded-md flex flex-col items-center justify-center relative overflow-hidden mx-auto py-10 md:py-0">
+      <div className="p-4 relative z-10 w-full text-center">
+        <Spotlight
+          className="-top-40 left-0 md:-top-20 md:left-60"
+          fill="white"
+        />
+        <h1 className="mt-20 md:mt-0 text-4xl md:text-7xl font-bold bg-clip-text text-transparent bg-gradient-to-r from-neutral-50 to-neutral-400">
+          Master the art of music
+        </h1>
+        <p className="mt-4 font-normal text-base md:text-lg text-neutral-300 max-w-lg mx-auto">
+          Dive into our comprehensive music courses and transform your musical
+          journey today. Whether you&apos;re a beginner or a seasoned musician,
+          our courses are designed to empower you with the knowledge and skills
+          to excel in the world of music.
+        </p>
+        <div className="my-4">
+          <Link href="/courses">
+            <Button
+              borderRadius="1.75rem"
+              className="bg-white dark:bg-slate-900 text-black dark:text-white border-neutral-200 dark:border-slate-800"
+            >
+              Explore Courses
+            </Button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default HeroSection;
