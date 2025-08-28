790a2cf8afbaef3534a409aaebdcdc629b68c8b0
Muhammad Sabbir Hossain Bin Ahmad <dustobalok000@gmail.com>
2025-08-28 08:51:00 +0600

    Complete: Solving Navbar challenge

--- musicnextjs/src/app/chai/lemon/page.tsx
+++
@@ -1,5 +0,0 @@
-function page() {
-  return <h1 className="text-3xl text-center mt-8">Lemon page</h1>;
-}
-
-export default page;
--- musicnextjs/src/app/chai/page.tsx
+++
@@ -1,8 +0,0 @@
-
-function page() {
-  return (
-    <h1 className="text-3xl text-center mt-8">Chai page</h1>
-  )
-}
-
-export default page
